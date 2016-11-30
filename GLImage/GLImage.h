6863ed8506c96cf1e02bb26133314d24c9c01103
jefffyangdis <fangyang@fangyangdemacbook-pro.local>
2016-11-30 10:39:47 +0800

    Initial Commit

---
+++ GLImage/GLImage.h
@@ -0,0 +1,13 @@
+//
+//  GLImage.h
+//  GLImage
+//
+//  Created by 方阳 on 16/11/30.
+//  Copyright © 2016年 jefffyang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GLImage : NSObject
+
+@end
---
+++ GLImage/GLImage.m
@@ -0,0 +1,13 @@
+//
+//  GLImage.m
+//  GLImage
+//
+//  Created by 方阳 on 16/11/30.
+//  Copyright © 2016年 jefffyang. All rights reserved.
+//
+
+#import "GLImage.h"
+
+@implementation GLImage
+
+@end
